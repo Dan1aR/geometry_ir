a87348deed9abc1b0bef6e0996a799b1bacd8112
Андрей Данилов <38531956+Dan1aR@users.noreply.github.com>
2025-09-23 14:53:53 +0300

    Expose GeoScript-to-TikZ prompt

--- geoscript_ir/__init__.py
+++ geoscript_ir/__init__.py
@@ -5,6 +5,7 @@ from .consistency import check_consistency
 from .printer import print_program
 from .ast import Program, Stmt, Span
 from .reference import BNF, LLM_PROMPT, get_llm_prompt
+from .reference_tikz import GEOSCRIPT_TO_TIKZ_PROMPT
 from .solver import (
     translate,
     solve,
@@ -21,5 +22,6 @@ __all__ = [
     'parse_program', 'validate', 'ValidationError', 'desugar', 'desugar_variants', 'check_consistency', 'print_program',
     'translate', 'solve', 'solve_best_model', 'solve_with_desugar_variants',
     'SolveOptions', 'Model', 'Solution', 'VariantSolveResult', 'normalize_point_coords',
-    'Program', 'Stmt', 'Span', 'BNF', 'LLM_PROMPT', 'get_llm_prompt'
+    'Program', 'Stmt', 'Span', 'BNF', 'LLM_PROMPT', 'get_llm_prompt',
+    'GEOSCRIPT_TO_TIKZ_PROMPT'
 ]
